7612e369daa251e55d0d3554f7f0fd37005e417f
Ranjitha <ranjitha@jumisa.io>
2025-03-07 14:30:40 +0530

    added

--- react-hooks-frontend/src/services/EmployeeService.js
+++ react-hooks-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_BASE_REST_API_URL = 'http://localhost:8080/api/v1/employees';
+const EMPLOYEE_BASE_REST_API_URL = 'https://backend-container.victorioussand-9982e61e.centralindia.azurecontainerapps.io:8080/api/v1/employees';
 
 class EmployeeService{
 
